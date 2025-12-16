8177dfb46988e236a3730cbb7cd731c0b8adc568
Saurav Bhasin <128213153+bhasinsaurav@users.noreply.github.com>
2025-12-16 01:49:58 +0000

    Creating objects and call methods

--- Start/Ch 1/definition_start.py
+++ Start/Ch 1/definition_start.py
@@ -3,9 +3,14 @@
 
 
 # TODO: create a basic class
-
+class Book:
+  def __init__(self, title):
+    self.title = title
 
 # TODO: create instances of the class
-
+book1 = Book("New Habits")
+book2 = Book("System Architecture")
 
 # TODO: print the class and property
+print(book1)
+print(book1.title)
